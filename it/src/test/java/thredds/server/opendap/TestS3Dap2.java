2621a6e1a649139eb468eecee9357611e2fc385d
Sean Arms <67096+lesserwhirls@users.noreply.github.com>
2020-03-04 14:18:08 -0700

    Add S3 catalog with at least one test

---
+++ it/src/test/java/thredds/server/opendap/TestS3Dap2.java
@@ -0,0 +1,26 @@
+package thredds.server.opendap;
+
+import java.io.IOException;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import thredds.TestOnLocalServer;
+import ucar.nc2.dataset.NetcdfDataset;
+import ucar.nc2.dataset.NetcdfDatasets;
+import ucar.unidata.util.test.category.NeedsExternalResource;
+
+public class TestS3Dap2 {
+
+  @Test
+  @Category(NeedsExternalResource.class)
+  public void testS3Dap2() throws IOException {
+    String filename = TestOnLocalServer.withHttpPath(
+        "dodsC/s3-test/ABI-L1b-RadC/2019/363/21/OR_ABI-L1b-RadC-M6C16_G16_s20193632101189_e20193632103574_c20193632104070.nc");
+    NetcdfDataset ncd = NetcdfDatasets.openDataset(filename, true, null);
+    Assert.assertNotNull(ncd);
+    Assert.assertEquals(44, ncd.getVariables().size());
+    Assert.assertEquals(9, ncd.getCoordinateAxes().size());
+    Assert.assertEquals(6, ncd.getCoordinateSystems().size());
+  }
+
+}
