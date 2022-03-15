add04beda266791fc768fba19a241fe1a5e21674
Tara Drwenski <tdrwenski@ucar.edu>
2022-03-15 11:15:05 -0600

    Update s3 catalog tests

--- tds/src/test/java/thredds/server/catalog/TestS3.java
+++ tds/src/test/java/thredds/server/catalog/TestS3.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.util.List;
 
 public class TestS3 {
-  private static final String CATALOG = "../tds/src/test/content/thredds/catalogS3.xml";
+  private static final String CATALOG = "../tds/src/test/content/thredds/tds-s3.xml";
 
   @Test
   public void shouldCreateCatalogWithS3Data() throws IOException {
@@ -16,17 +16,18 @@ public class TestS3 {
     Assert.assertNotNull(catalog);
 
     final List<Dataset> datasets = catalog.getDatasetsLocal();
-    Assert.assertEquals(3, datasets.size());
+    Assert.assertFalse(datasets.isEmpty());
 
-    final Dataset s3Dataset = datasets.get(0);
-    Assert.assertEquals("S3 Dataset", s3Dataset.getName());
+    final Dataset s3Dataset = catalog.findDatasetByName("S3 Dataset");
     Assert.assertEquals("s3-thredds-test-data/ncml/nc/namExtract/20060925_0600.nc", s3Dataset.getUrlPath());
 
-    final Dataset aggregation = datasets.get(1);
-    Assert.assertEquals("S3 Example NcML Aggregation", aggregation.getName());
+    final Dataset aggregation = catalog.findDatasetByName("S3 Example NcML Aggregation");
     Assert.assertEquals("S3ExampleNcML/Agg.nc", aggregation.getUrlPath());
 
-    final Dataset featureCollection = datasets.get(2);
-    Assert.assertEquals("S3 Feature Collection", featureCollection.getName());
+    final Dataset featureCollectionWithFolders = catalog.findDatasetByName("S3 Feature Collection With Folders");
+    Assert.assertNotNull(featureCollectionWithFolders);
+
+    final Dataset featureCollectionWithoutFolders = catalog.findDatasetByName("S3 Feature Collection Without Folders");
+    Assert.assertNotNull(featureCollectionWithoutFolders);
   }
 }
