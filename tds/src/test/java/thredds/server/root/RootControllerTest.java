88cbc4d991ffd4525120e6bb2aee53abea52ab18
Tara Drwenski <tdrwenski@ucar.edu>
2022-12-20 08:47:41 -0700

    Use google truth asserts

--- tds/src/test/java/thredds/server/root/RootControllerTest.java
+++ tds/src/test/java/thredds/server/root/RootControllerTest.java
@@ -1,7 +1,6 @@
 package thredds.server.root;
 
-import static org.junit.Assert.assertEquals;
-import org.junit.Assert;
+import static com.google.common.truth.Truth.assertThat;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -44,8 +43,8 @@ public class RootControllerTest {
     requestBuilder = MockMvcRequestBuilders.get("/");
     MvcResult mvc = this.mockMvc.perform(requestBuilder).andReturn();
     // Check that "/" is redirected
-    assertEquals(302, mvc.getResponse().getStatus());
-    assertEquals("redirect:/catalog/catalog.html", mvc.getModelAndView().getViewName());
+    assertThat(mvc.getResponse().getStatus()).isEqualTo(302);
+    assertThat(mvc.getModelAndView().getViewName()).isEqualTo("redirect:/catalog/catalog.html");
   }
 
   @Test
@@ -53,7 +52,7 @@ public class RootControllerTest {
     requestBuilder = MockMvcRequestBuilders.get("/tdsCat.css");
     MvcResult mvc = this.mockMvc.perform(requestBuilder).andReturn();
     // Check that "/" is redirected
-    Assert.assertEquals(200, mvc.getResponse().getStatus());
+    assertThat(mvc.getResponse().getStatus()).isEqualTo(200);
     String content = mvc.getResponse().getContentAsString();
     System.out.printf("content='%s'%n", content);
     // Assert.assertNotNull(mvc.getModelAndView());
