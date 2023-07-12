d8dd715cc3c3efd9ce2f8eef18b7a4900d03a9bc
haileyajohnson <hailey.johnson@ufl.edu>
2023-07-12 16:43:29 -0400

    add missing file from last commit

--- tds/src/main/java/thredds/server/exception/RequestTooLargeException.java
+++ tds/src/main/java/thredds/server/exception/RequestTooLargeException.java
@@ -7,8 +7,14 @@ package thredds.server.exception;
 
 public class RequestTooLargeException extends RuntimeException {
 
+  private static final String MESSAGE_FORMAT = "Requested %d, max size = %d";
+
   public RequestTooLargeException(String message) {
     super(message);
+  }
+
+  public RequestTooLargeException(long requested, long maxBytes) {
+    super(String.format(MESSAGE_FORMAT, requested, maxBytes));
   }
 
   public RequestTooLargeException(String message, Exception cause) {
