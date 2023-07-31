54758fa09728b9a1577e620b3b7400c72f0c3daf
Tara Drwenski <tdrwenski@ucar.edu>
2023-07-31 16:43:29 -0600

    Use valid_min instead of standardize to test ncml enhancements are used

--- tds/src/integrationTests/java/opendap/test/TestNcmlWithOpendap.java
+++ tds/src/integrationTests/java/opendap/test/TestNcmlWithOpendap.java
@@ -4,7 +4,6 @@ import static com.google.common.truth.Truth.assertThat;
 import static ucar.ma2.MAMath.nearlyEquals;
 
 import java.io.IOException;
-import java.math.RoundingMode;
 import java.nio.charset.StandardCharsets;
 import java.text.DecimalFormat;
 import java.util.Arrays;
@@ -18,15 +17,14 @@ import ucar.nc2.dataset.NetcdfDatasets;
 import ucar.unidata.util.test.TestDir;
 
 public class TestNcmlWithOpendap {
-  private static final double[] expectedValues =
-      new double[] {-1.161895003862225, -0.3872983346207417, 0.3872983346207417, 1.161895003862225};
+  private static final double[] expectedValues = new double[] {Double.NaN, 1, 2, 3};
 
   @Test
   public void shouldUseNcmlToModifyDatasetScanValues() throws IOException {
-    final String path = "/thredds/dodsC/StandardizeDatasetScan/testgrid1.nc";
+    final String path = "/thredds/dodsC/EnhancedDatasetScan/testgrid1.nc";
     final String url = "dods://" + TestDir.remoteTestServer + path;
     try (NetcdfFile ncFile = NetcdfDatasets.openDataset(url)) {
-      final Variable var = ncFile.findVariable("standardizedVar");
+      final Variable var = ncFile.findVariable("enhancedVar");
       assertThat((Object) var).isNotNull();
       final Array values = var.read();
       assertThat(nearlyEquals(values, Array.makeFromJavaArray(expectedValues))).isTrue();
@@ -35,13 +33,12 @@ public class TestNcmlWithOpendap {
 
   @Test
   public void shouldUseNcmlToModifyDatasetScanAsciiValues() {
-    final String path = "/dodsC/StandardizeDatasetScan/testgrid1.nc.ascii?standardizedVar%5B0:1:1%5D%5B0:1:1%5D";
+    final String path = "/dodsC/EnhancedDatasetScan/testgrid1.nc.ascii?enhancedVar%5B0:1:1%5D%5B0:1:1%5D";
     final String endpoint = TestOnLocalServer.withHttpPath(path);
 
     final byte[] content = TestOnLocalServer.getContent(endpoint, HttpServletResponse.SC_OK);
     final String stringContent = new String(content, StandardCharsets.UTF_8);
-    final DecimalFormat df = new DecimalFormat("#.###");
-    df.setRoundingMode(RoundingMode.DOWN);
+    final DecimalFormat df = new DecimalFormat("#.#");
     Arrays.stream(expectedValues).forEach(d -> assertThat(stringContent).contains(df.format(d)));
   }
 }
