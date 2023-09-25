30e22a418bf5a1cae6109f42125221fe44e42e13
Tara Drwenski <tdrwenski@ucar.edu>
2023-09-25 11:42:48 -0600

    Update tests to skip comparing dates in output

--- tds/src/integrationTests/java/thredds/server/threddsIso/TestThreddsIsoServices.java
+++ tds/src/integrationTests/java/thredds/server/threddsIso/TestThreddsIsoServices.java
@@ -4,9 +4,11 @@ import static com.google.common.truth.Truth.assertWithMessage;
 
 import javax.servlet.http.HttpServletResponse;
 import org.junit.Test;
+import org.w3c.dom.Node;
 import org.xmlunit.builder.DiffBuilder;
 import org.xmlunit.builder.Input;
 import org.xmlunit.diff.Diff;
+import org.xmlunit.util.Predicate;
 import thredds.test.util.TestOnLocalServer;
 import thredds.util.ContentType;
 
@@ -15,32 +17,36 @@ public class TestThreddsIsoServices {
   public void shouldReturnNcml() {
     final String path = "ncml/scanLocal/testgrid1.nc";
     final String expectedOutput = "testgrid1.ncml.xml";
-    compare(path, expectedOutput, ContentType.xml);
+    final Predicate<Node> filter = node -> !(node.hasAttributes() && node.getAttributes().getNamedItem("name") != null
+        && node.getAttributes().getNamedItem("name").getNodeValue().equals("metadata_creation"));
+    compare(path, expectedOutput, ContentType.xml, filter);
   }
 
   @Test
   public void shouldReturnIso() {
     final String path = "iso/scanLocal/testgrid1.nc";
     final String expectedOutput = "testgrid1.iso.xml";
-    compare(path, expectedOutput, ContentType.xml);
+    final Predicate<Node> filter =
+        node -> !node.getTextContent().startsWith("This record was translated from NcML using")
+            && !node.getNodeName().startsWith("gco:Date");
+    compare(path, expectedOutput, ContentType.xml, filter);
   }
 
   @Test
   public void shouldReturnUddc() {
     final String path = "uddc/scanLocal/testgrid1.nc";
     final String expectedOutput = "testgrid1.uddc.html";
-    compare(path, expectedOutput, ContentType.html);
+    compare(path, expectedOutput, ContentType.html, node -> true);
   }
 
-  private void compare(String path, String expectedOutputFilename, ContentType expectedType) {
+  private void compare(String path, String expectedOutputFilename, ContentType expectedType, Predicate<Node> filter) {
     final String endpoint = TestOnLocalServer.withHttpPath(path);
     final byte[] response = TestOnLocalServer.getContent(endpoint, HttpServletResponse.SC_OK, expectedType);
 
     final Diff diff = DiffBuilder.compare(Input.fromStream(getClass().getResourceAsStream(expectedOutputFilename)))
         .withTest(Input.fromByteArray(response)).ignoreComments().normalizeWhitespace()
-        // don't compare iso element with version and current datetime in it
-        .withNodeFilter(node -> !node.getTextContent().startsWith("This record was translated from NcML using"))
-        .build();
+        // don't compare elements with e.g. version/ current datetime
+        .withNodeFilter(filter).build();
     assertWithMessage(diff.toString()).that(diff.hasDifferences()).isFalse();
   }
 }
