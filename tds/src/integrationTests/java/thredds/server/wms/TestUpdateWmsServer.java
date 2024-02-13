1d1f99a7e5c571bcd476e0fd5fdd7a3650f532c8
Tara Drwenski <tdrwenski@ucar.edu>
2024-02-13 09:19:54 -0700

    Use temporary folder for wms test

--- tds/src/integrationTests/java/thredds/server/wms/TestUpdateWmsServer.java
+++ tds/src/integrationTests/java/thredds/server/wms/TestUpdateWmsServer.java
@@ -5,6 +5,7 @@
 
 package thredds.server.wms;
 
+import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -19,9 +20,9 @@ import org.jdom2.filter.Filters;
 import org.jdom2.input.SAXBuilder;
 import org.jdom2.xpath.XPathExpression;
 import org.jdom2.xpath.XPathFactory;
-import org.junit.After;
-import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.io.IOException;
@@ -39,27 +40,32 @@ public class TestUpdateWmsServer {
   private static final Namespace NS_WMS = Namespace.getNamespace("wms", "http://www.opengis.net/wms");
 
   private static final String DIR = "src/test/content/thredds/public/testdata/";
-  private static final Path TEST_FILE = Paths.get(DIR, "testUpdate.nc");
+  private static final Path SRC_FILE_1 = Paths.get(DIR, "testGridAsPoint.nc");
+  private static final Path SRC_FILE_2 = Paths.get(DIR, "testData.nc");
+  private static String FILENAME = "testUpdate.nc";
 
-  @After
-  public void cleanupTestFile() throws IOException {
-    Files.delete(TEST_FILE);
-  }
+  @Rule
+  public TemporaryFolder temporaryFolder = new TemporaryFolder(new File(DIR));
 
   @Test
   public void testUpdateFile() throws IOException, JDOMException {
-    final String path = "/wms/localContent/testUpdate.nc?service=WMS&version=1.3.0&request=GetCapabilities";
+    final String filePath = temporaryFolder.getRoot().getName() + "/" + FILENAME;
+    final String path = "/wms/localContent/" + filePath + "?service=WMS&version=1.3.0&request=GetCapabilities";
     final String endpoint = TestOnLocalServer.withHttpPath(path);
 
     // Check initial WMS output
-    Files.copy(Paths.get(DIR, "testGridAsPoint.nc"), TEST_FILE);
+    createTestFile(SRC_FILE_1, FILENAME);
     final byte[] result = TestOnLocalServer.getContent(endpoint, HttpServletResponse.SC_OK, ContentType.xmlwms);
     checkLayerNameInXml(result, "withT1Z1");
 
     // Update test file and check that WMS output is updated
-    Files.copy(Paths.get(DIR, "testData.nc"), TEST_FILE, StandardCopyOption.REPLACE_EXISTING);
+    createTestFile(SRC_FILE_2, FILENAME);
     final byte[] updatedResult = TestOnLocalServer.getContent(endpoint, HttpServletResponse.SC_OK, ContentType.xmlwms);
     checkLayerNameInXml(updatedResult, "Z_sfc");
+  }
+
+  private void createTestFile(Path srcFile, String filename) throws IOException {
+    Files.copy(srcFile, Paths.get(temporaryFolder.getRoot().toString(), filename), StandardCopyOption.REPLACE_EXISTING);
   }
 
   private void checkLayerNameInXml(byte[] result, String expectedLayerName) throws IOException, JDOMException {
