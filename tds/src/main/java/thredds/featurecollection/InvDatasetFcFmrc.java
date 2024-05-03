28873f2dcfa8392321ee1a392775b7f0e28c1307
Tara Drwenski <tdrwenski@ucar.edu>
2024-05-03 09:49:51 -0500

    Remove dead code branch-- new NetcdfDataset never null

--- tds/src/main/java/thredds/featurecollection/InvDatasetFcFmrc.java
+++ tds/src/main/java/thredds/featurecollection/InvDatasetFcFmrc.java
@@ -472,7 +472,7 @@ public class InvDatasetFcFmrc extends InvDatasetFeatureCollection {
         // the new API, that way we get any new RandomAccessFile implementations that rely
         // on the new API (e.g. S3RandomAccessFile).
         NetcdfDataset ncd = new NetcdfDataset(ncdNew.getReferencedFile(), null);
-        return ncd == null ? null : new ucar.nc2.dt.grid.GridDataset(ncd);
+        return new ucar.nc2.dt.grid.GridDataset(ncd);
       } else if (wantName.equals(hasName + FMRC)
           && wantDatasets.contains(FeatureCollectionConfig.FmrcDatasetType.TwoD)) {
         return fmrc.getDataset2D(null);
